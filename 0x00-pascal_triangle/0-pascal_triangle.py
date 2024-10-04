6db570fafd49a7dab2e984886d011ba6e66160f5
abbeypentious@gmail.com <abbeypentious@gmail.com>
2024-10-03 17:13:14 -0700

    update

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
+"""
+0-pascal_triangle
+"""
+
 
 def pascal_triangle(n):
+    """
+    Returns a list of integers
+    representing the Pascal Triangle of n
+    returns empty list if n <= 0
+    """
+    k = []
     if n <= 0:
-        return []
-    
-    triangle = [[1]]  # Initialize the first row of the Pascal's triangle
-
+        return k
+    k = [[1]]
     for i in range(1, n):
-        prev_row = triangle[-1]
-        new_row = [1]  # First element of each row is always 1
-        
-        # Generate the middle elements of the row
-        for j in range(1, i):
-            new_row.append(prev_row[j-1] + prev_row[j])
-        
-        new_row.append(1)  # Last element of each row is always 1
-        triangle.append(new_row)
-    
-    return triangle
+        temp = [1]
+        for j in range(len(k[i - 1]) - 1):
+            curr = k[i - 1]
+            temp.append(k[i - 1][j] + k[i - 1][j + 1])
+        temp.append(1)
+        k.append(temp)
+    return k
